b9052d7ed09b53ea73c50141a28501a3238edc29
Merek Orlowski <merekorlowski@gmail.com>
2017-04-11 14:10:48 -0400

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-/**
- * Remplacer 'postgress:password@localhost' a votre nom d'usager et votre password
- */
-
-module.exports = {
-    dbUrl: 'postgres://postgres:password@localhost:5432/tournois_sports'
-}
